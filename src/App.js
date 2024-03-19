30e181eda64287d1ebb92e6d5e71814d20cb28c3
LukaDudauri7 <lukadudauri01@gmail.com>
2024-03-19 10:23:30 +0400

    data.js updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useState  } from 'react';
+
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton)
+}
   return (
     <div>
       <Header />
@@ -19,6 +27,23 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <p></p>
+            <pre>
+              <code>
+                
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
